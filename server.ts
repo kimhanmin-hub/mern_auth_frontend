d6bdad1bbb98ebdea7023449172e1a932c2c53a4
kimhanmin <cjswogksals@naver.com>
2024-11-11 15:07:58 +0900

    fix

--- server.ts
+++ server.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+export const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000/api/v1';
 console.log('API_URL:', API_URL); 
